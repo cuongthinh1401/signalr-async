319b4d64dd6c68280a60471f90c38a973c35b2b1
cuongthinh1401 <43697415+cuongthinh1401@users.noreply.github.com>
2024-05-17 11:47:07 +0700

    add stop connection on ConnectionClosed

--- signalr_async/client.py
+++ signalr_async/client.py
@@ -174,6 +174,7 @@ class SignalRClientBase(ABC, Generic[R, I]):
                             await self._connection.ping()
                         except ConnectionClosed:
                             self.logger.error("Connection keepalive failed to send")
+                            await self._stop_connection()
                     else:
                         await asyncio.sleep(self.keepalive_interval - diff)
             except asyncio.CancelledError:
